51b88c13cf41048c2ccee6d362b55635ed3aec0b
@Nagmanicodes <patilnagmani1@gmail.com>
2025-11-16 10:52:48 +0530

    Add files via upload

---
+++ HelloGoal.java
@@ -0,0 +1,11 @@
+
+//Write a Java program that prints this output dynamically using your name and your goal
+public class HelloGoal {
+    public static void main(String[] args) {
+    String name = "Nagmani";
+    String goal= "SDE";
+    System.out.println(" My name is  "+name+"");
+    System.out.println("My goal is "+ goal +"");
+}
+
+}
