873a4e5215a636fa6b8ed028637c006a5fece3be
@Nagmanicodes <patilnagmani1@gmail.com>
2025-11-16 10:55:26 +0530

    Add files via upload

---
+++ MixTest.java
@@ -0,0 +1,7 @@
+//Predict the output and explain:
+public class MixTest  {
+    public static void main(String[] args) {
+        System.out.println("Result: " + 10 + 20);
+        System.out.println(10 + 20 + " Result");
+    }
+}
